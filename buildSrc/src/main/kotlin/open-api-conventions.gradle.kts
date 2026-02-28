3ff113bb868c0ff0e54f613987c4914f4cf0f362
U-235-92 <a745296@yandex.ru>
2026-02-28 13:15:12 +0400

    Updated buildSrc module .gradle build script files

--- buildSrc/src/main/kotlin/open-api-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/open-api-conventions.gradle.kts
@@ -1,5 +1,30 @@
+import gradle.kotlin.dsl.accessors._0430f2283e40a71ba65f409311bb6762.openApiGenerate
+import gradle.kotlin.dsl.accessors._d7c1cb8291fcf7e869bfba85a0dc6ae2.implementation
+
 plugins {
     id("org.openapi.generator")
+}
+
+val dependencyVersionMap = mapOf(
+    "feign-core" to "13.6",
+    "feign-jackson" to "13.6",
+    "feign-slf4j" to "13.6",
+    "feign-form-spring" to "3.8.0",
+    "jakarta-annotation" to "3.0.0",
+    "jakarta-validation" to "3.1.1"
+)
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.core:jackson-core")
+    implementation("com.fasterxml.jackson.core:jackson-annotations")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("io.github.openfeign:feign-core:${dependencyVersionMap.getValue("feign-core")}")
+    implementation("io.github.openfeign:feign-jackson:${dependencyVersionMap.getValue("feign-jackson")}")
+    implementation("io.github.openfeign:feign-slf4j:${dependencyVersionMap.getValue("feign-slf4j")}")
+    implementation("io.github.openfeign.form:feign-form-spring:${dependencyVersionMap.getValue("feign-form-spring")}")
+    implementation("jakarta.annotation:jakarta.annotation-api:${dependencyVersionMap.getValue("jakarta-annotation")}")
+    implementation("jakarta.validation:jakarta.validation-api:${dependencyVersionMap.getValue("jakarta-validation")}")
 }
 
 openApiGenerate {
